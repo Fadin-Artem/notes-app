6ce4f0fc956a504bb75a592f6494d75b6759f4fa
Fadin Artem <a.fadin-forwork@mail.ru>
2024-01-15 16:52:03 +0600

    add project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-       
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/about" component={Note} />
+      </Switch>
+    </Router>
     </div>
   );
 }
